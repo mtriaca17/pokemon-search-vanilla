7e1b1b1cec3c775b25af1935c8dbe13edeb9082b
Miguel <migueltriaca@gmail.com>
2021-06-23 21:53:16 +0800

    changed something to const

--- practice.js
+++ practice.js
@@ -8,29 +8,29 @@ const searchInput = document.querySelector('#searchInput');
 searchForm.addEventListener('submit', e => {
   e.preventDefault();
   const searchQuery = searchInput.value.toLowerCase();
-  fetchPokemon(searchQuery)
-})
+  fetchPokemon(searchQuery);
+});
 
 randomBtn.addEventListener('click', e => {
-  fetchPokemon(randNum())
-})
+  fetchPokemon(randNum());
+});
 
 //data functions
-const fetchPokemon = async (id) => {
+const fetchPokemon = async id => {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
   const data = await res.json();
-  return getPokeData(data) 
-}
+  return getPokeData(data);
+};
 
 function getPokeData(data) {
   //id, name, type
-  pokeInfo = {
+  const pokeInfo = {
     id: data.id,
     name: data.name,
     type: getTypes(data.types),
-    sprites: getSprites(data.sprites)
-  }
-  addDataToPage(pokeInfo)
+    sprites: getSprites(data.sprites),
+  };
+  addDataToPage(pokeInfo);
   // return pokeInfo
 }
 
@@ -39,19 +39,19 @@ function getPokeData(data) {
 function addDataToPage(data) {
   // console.log(data)
   container.innerHTML = '';
-  searchInput.value = data.id
+  searchInput.value = data.id;
   container.append(createCard(data.name, data.id, data.sprites, data.type));
 }
 
 function createCard(name, id, sprites, type) {
   const cardContainer = document.createElement('div');
   cardContainer.classList.add('card-container');
-  
-  cardContainer.append(createNameDiv(name, id))
+
+  cardContainer.append(createNameDiv(name, id));
   cardContainer.append(createSpritesDiv(sprites));
   cardContainer.append(createTypeDiv(type));
 
-  return cardContainer
+  return cardContainer;
 }
 
 function createNameDiv(name, id) {
@@ -59,24 +59,26 @@ function createNameDiv(name, id) {
   nameDiv.classList.add('name-container');
 
   nameDiv.innerHTML = `
-  <h3 class="poke-id">${id}</h3> <span class="name-dash">-</span> <h3 class="poke-name">${capitalizeFirstLetter(name)}</h3>
-  `
+  <h3 class="poke-id">${id}</h3> <span class="name-dash">-</span> <h3 class="poke-name">${capitalizeFirstLetter(
+    name
+  )}</h3>
+  `;
 
-  return nameDiv
+  return nameDiv;
 }
 
 function createSpritesDiv(sprites) {
   const cardImages = document.createElement('div');
-  cardImages.classList.add('image-container')
+  cardImages.classList.add('image-container');
 
   sprites.forEach(item => {
     img = document.createElement('img');
-    img.classList.add('sprite')
+    img.classList.add('sprite');
     img.src = item;
-    cardImages.append(img)
-  })
+    cardImages.append(img);
+  });
 
-  return cardImages;  
+  return cardImages;
 }
 
 function createTypeDiv(type) {
@@ -85,10 +87,10 @@ function createTypeDiv(type) {
 
   type.forEach(item => {
     type = document.createElement('p');
-    type.classList.add(item)
-    type.innerText = capitalizeFirstLetter(item)
-    typeContainer.append(type)
-  })
+    type.classList.add(item);
+    type.innerText = capitalizeFirstLetter(item);
+    typeContainer.append(type);
+  });
 
   return typeContainer;
 }
@@ -100,17 +102,17 @@ function getSprites(obj) {
 
   spriteUrlArr.push(obj.front_default);
   spriteUrlArr.push(obj.front_shiny);
-  
+
   return spriteUrlArr;
 }
 
 function getTypes(arr) {
-  outputTypes = []
-  
-  arr.forEach(item=> {
-    outputTypes.push(item.type.name)
+  outputTypes = [];
+
+  arr.forEach(item => {
+    outputTypes.push(item.type.name);
   });
-  
+
   return outputTypes;
 }
 
